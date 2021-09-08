198ea33fb3ef10db3759c5404819534840023aac
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2021-09-08 23:18:09 +0900

    first comitt

---
+++ src/test/java/YIcharingsystem20210826/charing_system/CharingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package YIcharingsystem20210826.charing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
